601bb4f17458bf10bacea779e586c55b9c59ef7a
Arthur <a.kalinowska@students.ephec.be>
2023-04-22 12:51:56 +0200

    modif db suite

--- Intranet/DB/test.sql
+++ Intranet/DB/test.sql
@@ -2,26 +2,15 @@
 -- DROP TABLE IF EXISTS Customers;
 /!40101 SET @saved_cs_client     = @@character_set_client/;
 /!50503 SET character_set_client = utf8mb4/;
-CREATE DATABASE b2b;
-USE b2b;
+CREATE DATABASE db;
+USE db;
 CREATE TABLE inventory (id INT AUTO_INCREMENT PRIMARY KEY,name varchar(255) DEFAULT NULL,price FLOAT(10) DEFAULT NULL);
 
-INSERT INTO inventory VALUES (NULL, 'buzz' , 11.50);
-INSERT INTO inventory VALUES (NULL, 'woody' , 5.36);
-INSERT INTO inventory VALUES (NULL, 'playmobil' , 9.99);
+INSERT INTO inventory VALUES (NULL, 'PS5' , 11.50);
+INSERT INTO inventory VALUES (NULL, 'Balancoire' , 5.36);
+INSERT INTO inventory VALUES (NULL, 'Dames' , 9.99);
 
-CREATE USER 'userb2b'@'%' IDENTIFIED BY 'Zh0qtDbBRiKsmX4V8dES';
+CREATE USER 'userdb'@'%' IDENTIFIED BY 'Zh0qtDbBRiKsmX4V8dES';
  
-GRANT ALL PRIVILEGES ON b2b.* TO 'userb2b'@'%';
+GRANT ALL PRIVILEGES ON db.* TO 'userdb'@'%';
 
-CREATE DATABASE internal;
-USE internal;
-CREATE TABLE employee (id INT AUTO_INCREMENT PRIMARY KEY,name varchar(255) DEFAULT NULL,surname varchar(255) DEFAULT NULL, email varchar(255) DEFAULT NULL);
-
-INSERT INTO employee VALUES (NULL, 'Jepson' , 'Noham', 'aa@gmail.com');
-INSERT INTO employee VALUES (NULL, 'Jerome' , 'Liam', 'bb@gmail.com');
-INSERT INTO employee VALUES (NULL,  'Jarrett' , 'Den', 'cc@gmail.com');
-
-CREATE USER 'userinternal'@'%' IDENTIFIED BY '9Y8WlgYjPzZznK5SMn2Z';
- 
-GRANT ALL PRIVILEGES ON internal.* TO 'userinternal'@'%';
