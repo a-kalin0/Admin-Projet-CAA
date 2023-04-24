ad7b2c89bd121c3091169134fe112fc46861cc32
Arthur <a.kalinowska@students.ephec.be>
2023-04-24 17:11:40 +0200

    modif index.html en index.php + ajout connexion à la db

---
+++ Intranet/web/index.php
@@ -0,0 +1,37 @@
+<?php echo 'Bienvenue sur le site de Woodytoys !'; ?>
+
+<?php
+
+// Connexion à la base de données
+$servername = "localhost";
+$username = "user";
+$password = "password";
+$dbname = "database";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Vérification de la connexion
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Récupération des données de la table "inventory"
+$sql = "SELECT * FROM inventory";
+$result = $conn->query($sql);
+
+// Affichage des données dans un tableau HTML
+if ($result->num_rows > 0) {
+  echo "<table>";
+  echo "<tr><th>ID</th><th>Nom</th><th>Prix</th></tr>";
+  while($row = $result->fetch_assoc()) {
+    echo "<tr><td>" . $row["id"] . "</td><td>" . $row["name"] . "</td><td>" . $row["price"] . "</td></tr>";
+  }
+  echo "</table>";
+} else {
+  echo "0 résultats";
+}
+
+// Fermeture de la connexion
+$conn->close();
+?>
+
